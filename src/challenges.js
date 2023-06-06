167e361cd1170304c664cae0749d49f544b0acaa
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-06 11:36:54 -0300

    Criada Função splitSentences

--- src/challenges.js
+++ src/challenges.js
@@ -6,39 +6,41 @@ const guestsDatabase = require('./data.json');
 // =================================================
 
 // Requisito 1 - Crie uma função que divida uma frase
-
+function splitSentence(phrase) {
+  return phrase.split(' ');
+}
 // Requisito 2 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
-
+function footballPoints() {}
 // Requisito 3 - Crie uma função que adiciona músicas em uma playlist
-
+function addMusics(){}
 // =================================================
 // PARTE 2
 // =================================================
 
 // Requisito 4 - Crie uma função que retorna o produto mais caro de acordo com uma categoria
-
+function moreExpensive(){}
 // Requisito 5 - Crie uma função que verifica se um determinado item já existe
-
+function checkItem(){}
 // Requisito 6 - Crie uma função que adiciona um novo item caso ele ainda não exista
-
+function addNewItem(){}
 // Requisito 7 - Crie uma função que conta a quantidade de pessoas por gênero
-
+function counterGender(){}
 // =================================================
 // PARTE 3
 // =================================================
 
 // Requisito 8 - Crie uma função que retorna os elementos de um determinado estado
-
+function filterState(){}
 // Requisito 9 - Crie uma função que altera a propriedade `picture`
-
+function changePicture(){}
 // Requisito 10 - Crie um função que gera um relatório
-
+function generateReport(){}
 // Não modifique as linhas abaixo
 module.exports = {
   splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
   footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
   addMusics: typeof addMusics === 'function' ? addMusics : (() => {}),
-  playlist,
+  playlist: typeof playlist === 'undefined' ? [] : playlist,
   moreExpensive: typeof moreExpensive === 'function' ? moreExpensive : (() => {}),
   checkItem: typeof checkItem === 'function' ? checkItem : (() => {}),
   addNewItem: typeof addNewItem === 'function' ? addNewItem : (() => {}),
